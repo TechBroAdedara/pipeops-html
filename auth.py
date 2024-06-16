86f58983ee567bb7bbb02a7ca4a1b453c0fb8ba9
Adedara <courageadedara@gmail.com>
2024-06-16 23:41:29 +0100

    Removed duplicate function in Auth.py

--- auth.py
+++ auth.py
@@ -98,23 +98,7 @@ def create_access_token(username: str, user_matric: str, expires_delta: timedelt
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        user_id: int = payload.get('id')
-        if username is None or user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail='Could not validate user.'
-            )
-        return {"username": username, 'id': user_id}
-        
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail='Could not validate user.'
-        )
+
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
     try:
